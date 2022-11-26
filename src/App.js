77d06fd9f563a19e0180f3f0493258a11aaacbe7
Alexandre Afonso <alexandreafonso@MacBook-Air-de-Alexandre.local>
2022-11-26 19:53:51 -0300

    anula projeto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import logo from '../src/logo.png'
-import Gmaps from './components/googlemap/gmap';
-import Header from './components/header';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import logo from '../src/logo.png'
+// import Gmaps from './components/googlemap/gmap';
+// import Header from './components/header';
 
 function App() {
   return (

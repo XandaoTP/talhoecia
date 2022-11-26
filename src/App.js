4737af516884c5919efa504e8d62bac5ad1f87fd
Alexandre Afonso <alexandreafonso@MacBook-Air-de-Alexandre.local>
2022-11-26 19:51:27 -0300

    anula projeto

--- src/App.js
+++ src/App.js
@@ -5,24 +5,7 @@ import Header from './components/header';
 
 function App() {
   return (
-    <>
-      <Header />
-      <div className='divLogo'>
-        <img src={logo} className='logo' alt='Talhoecia Logo'/>
-        <p className='text-white'>EM BREVE OS MELHORES CORTES DA CIDADE</p>
-      </div>
-      <footer className="text-center">
-        <div className="card-header">
-        Casa de carne
-        </div>
-        <div className="card-body">
-          <h5 className="card-title">TALHO&CIA</h5>
-          <p className="card-text">Av. da Saudade, 1257 - Santa Marta - Uberaba/MG</p>
-          <FontAwesomeIcon icon="fa-brands fa-square-whatsapp" />
-          <Gmaps />
-        </div>
-    </footer>
-  </>
+   <>...</>
   );
 }
 

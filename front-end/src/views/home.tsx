a213cedb8763fb509c1a69b1cd79c3504864e631
unknown <alexandre8603@gmail.com>
2023-02-25 13:54:34 -0300

    adjust media query

--- front-end/src/views/home.tsx
+++ front-end/src/views/home.tsx
@@ -4,7 +4,7 @@ export function Home () {
     return (
         <>
            <TopBar /> 
-           <section className="mt-5 flex bg-gray-300 justify-center h-96 rounded-2xl mx-60">
+           <section className="mt-10 flex bg-gray-300 justify-center h-96 rounded-2xl mx-60 xsm:flex xsm:w-50 xsm:mx-2 sm:mx-2">
                 <div>
                     <p className="text-bold">Ofertas</p>
                 </div>

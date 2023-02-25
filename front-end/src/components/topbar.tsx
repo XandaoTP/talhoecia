6acd831fb66090d12a0805560510283d19031fc1
unknown <alexandre8603@gmail.com>
2023-02-25 14:05:16 -0300

    Ajuste css topbar

--- front-end/src/components/topbar.tsx
+++ front-end/src/components/topbar.tsx
@@ -1,8 +1,8 @@
 export function TopBar () {
     return (
         <>
-            <header>
-                <div className="flex flex-row h-12 justify-center items-center shadow-sm shadow-yellow-700">
+            <header className="bg-opacity-60 bg-black h-14 shadow-md shadow-yellow-700">
+                <div className="flex flex-row h-12 justify-center items-center shadow-sm">
                     <h1 className="font-bold text-3xl text-white" >Talho e cia</h1>
                 </div>    
             </header>            
--- front-end/src/views/home.tsx
+++ front-end/src/views/home.tsx
@@ -4,9 +4,9 @@ export function Home () {
     return (
         <>
            <TopBar /> 
-           <section className="mt-10 flex bg-gray-300 justify-center h-96 rounded-2xl mx-60 xsm:flex xsm:w-50 xsm:mx-2 sm:mx-2">
+           <section className="mt-10 flex bg-gray-300 justify-center h-96 rounded-2xl mx-60 xsm:flex xsm:w-50 xsm:mx-2 sm:mx-2 bg-opacity-60">
                 <div>
-                    <p className="text-bold">Ofertas</p>
+                    <p className="text-bold ">Ofertas</p>
                 </div>
            </section>       
            

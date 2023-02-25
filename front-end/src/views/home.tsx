07e61b4aec6cd521708d1d146aae0945e8d6457f
unknown <alexandre8603@gmail.com>
2023-02-25 14:21:11 -0300

    add fontawesome

--- front-end/src/views/home.tsx
+++ front-end/src/views/home.tsx
@@ -5,8 +5,8 @@ export function Home () {
         <>
            <TopBar /> 
            <section className="mt-10 flex bg-gray-300 justify-center h-96 rounded-2xl mx-60 xsm:flex xsm:w-50 xsm:mx-2 sm:mx-2 bg-opacity-60">
-                <div>
-                    <p className="text-bold ">Ofertas</p>
+                <div className="flex justify-center items-center">
+                    <p className="text-bold ">Site em construção</p>
                 </div>
            </section>       
            

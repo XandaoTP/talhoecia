706dcec313179f7589599a3bb9c25db34528cc00
unknown <alexandre8603@gmail.com>
2023-02-25 14:36:01 -0300

    add sectionSite em construçao

--- front-end/src/views/home.tsx
+++ front-end/src/views/home.tsx
@@ -1,4 +1,7 @@
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { TopBar } from "../components/topbar";
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 export function Home () {
     return (
@@ -6,7 +9,9 @@ export function Home () {
            <TopBar /> 
            <section className="mt-10 flex bg-gray-300 justify-center h-96 rounded-2xl mx-60 xsm:flex xsm:w-50 xsm:mx-2 sm:mx-2 bg-opacity-60">
                 <div className="flex justify-center items-center">
-                    <p className="text-bold ">Site em construção</p>
+                <FontAwesomeIcon icon={faCoffee} className="text-emerald-700"/>
+                <p className="text-bold px-5 text-3xl">Site em construção</p>
+                <FontAwesomeIcon icon={faCoffee} className="text-emerald-700"/>
                 </div>
            </section>       
            
